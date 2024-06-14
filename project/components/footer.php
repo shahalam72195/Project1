217e3f16e1af50fa589b79fb411c6ec1d5f7d2f1
Shah Alam <158993604+shahalam72195@users.noreply.github.com>
2024-06-14 12:47:03 +0530

    Add files via upload

---
+++ project/components/footer.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="css/footer.css">
+<footer class="footer">
+   <div class="container">
+      <div class="footer-column">
+         <h3>Contact Us</h3>
+         <ul>
+            <li><a href="mailto:shahalam72195@gmail.com">Email : shahalam72195@gmail.com</a></li>
+            <li><a href="tel:+1234567890">Phone : +91 7455872195</a></li>
+            <li>Address : Sunderdeep Engineering College,Ghaziabad-201015</li>
+            <li>&copy; copyright @ <?= date('Y'); ?> by <span>Shah Alam</span> | all rights reserved!
+            </li>
+         </ul>
+      </div>
+      <div class="footer-column">
+         <h3>Follow Us On</h3>
+         <ul class="social-media">
+            <li><a href="https://www.facebook.com/profile.php?id=100013642663052&mibextid=ZbWKwL"><img src="images/facebook.png" alt="Facebook"></a></li>
+            <li><a href="https://x.com/iamshahalam95?t=uNDtIlQ0m8bpMYHfTgrysg&s=08"><img src="images/twitter.png" alt="Twitter"></a></li>
+            <li><a href="https://www.instagram.com/shah_alam_74?igsh=cGdpaXlsczFsMWU5"><img src="images/insta.jpg" alt="Instagram"></a></li>
+         </ul>
+      </div>
+
+      <div class="footer-column">
+         <h3>Quick Links</h3>
+         <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+         </ul>
+      </div>
+
+   </div>
+
+
+
+</footer>
+
+<!-- <div class="loader">
+   <img src="images/loader.gif" alt="">
+</div> -->
